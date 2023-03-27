0227434c5b609de1e20739bbdbcb11484f0e6426
@eutheuzin7 <79593777+eutheuzin7@users.noreply.github.com>
2023-03-27 00:23:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>online CHATBOT in PHP | OPENAI</title>
+    <title>Chatbot online em PHP | OpenAI</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
-<style>
-body {
-  background-image: url('https://i.pinimg.com/originals/14/02/10/140210aa340e49d85576e23f42ec15e6.gif');
-}
-</style>
+    <style>
+        body {
+          background-image: url('https://i.pinimg.com/originals/14/02/10/140210aa340e49d85576e23f42ec15e6.gif');
+        }
+    </style>
     <div class="wrapper">
-        <div class="title">chatbot web @EUTHEUZIN</div>
+        <div class="title">Chatbot web @EUTHEUZIN</div>
         <div class="form">
             <div class="bot-inbox inbox">
                 <div class="icon">
                     <i class="fas fa-user"></i>
                 </div>
                 <div class="msg-header">
-                    <p>Olá usuário, envie-me alguma coisa</p>
+                <p>Olá usuário, envie-me algo</p>
                 </div>
             </div>
         </div>
@@ -33,30 +33,30 @@ body {
             </div>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function(){
-            $("#send-btn").on("click", function(){
-                $value = $("#data").val();
-                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
-                $(".form").append($msg);
-                $("#data").val('');
-                
-                // start ajax code
-                $.ajax({
-                    url: 'openai.php',
-                    type: 'POST',
-                    data: 'text='+$value,
-                    success: function(result){
-                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
-                        $(".form").append($replay);
-                        // quando o chat desce a barra de rolagem chega automaticamente ao final
-                        $(".form").scrollTop($(".form")[0].scrollHeight);
-                    }
-                });
+    
+<script>
+    $(document).ready(function(){
+        $("#send-btn").on("click", function(){
+            $value = $("#data").val();
+            $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+            $(".form").append($msg);
+            $("#data").val('');
+            
+            // start ajax code
+            $.ajax({
+                url: 'openai.php',
+                type: 'POST',
+                data: 'text='+$value,
+                success: function(result){
+                    $reply = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                    $(".form").append($reply);
+                    // quando o chat desce a barra de rolagem chega automaticamente ao final
+                    $(".form").scrollTop($(".form")[0].scrollHeight);
+                }
             });
         });
-    </script>
-   //index personalizada por: @EUTHEUZIN
+    });
+</script>
+<!--Index personalizada por: @EUTHEUZIN-->
 </body>
 </html>
