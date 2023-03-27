2a3fca647055884eb617b369551bf3083168633d
@eutheuzin7 <79593777+eutheuzin7@users.noreply.github.com>
2023-03-27 00:24:58 -0300

    Update openai.php

--- openai.php
+++ openai.php
@@ -1,42 +1,54 @@
 <?php
 
-function getStr($string, $start, $end)
-{
-    $str = explode($start, $string);
-    $str = explode($end, $str[1]);
-    return $str[0];
+// Verifica se foi enviado um valor para a variável 'text'
+if (!isset($_POST['text']) || empty($_POST['text'])) {
+    echo 'Por favor, diga-me algo para que eu possa ajudar a sanar sua dúvida.';
+    exit;
 }
 
-$message = $_POST['text'];
-
+// Inclui o autoload do Composer
 require_once 'composer/vendor/autoload.php';
+
 use Orhanerday\OpenAi\OpenAi;
 
-$api = "sk-";
-$open_ai = new OpenAi($api);
+// Define a chave de API
+$api_key = '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';
 
-$complete = $open_ai->complete([
+// Cria uma instância da classe OpenAi
+$open_ai = new OpenAi($api_key);
+
+// Define os parâmetros da chamada à API
+$params = [
     'engine' => 'text-davinci-002',
-    'prompt' => ''.$message.'',
+    'prompt' => $_POST['text'],
     'temperature' => 0.7,
     'max_tokens' => 100,
     'frequency_penalty' => 0,
-    'presence_penalty' => 0.6
-]);
+    'presence_penalty' => 0.6,
+];
 
-// $string = json_decode($complete); pegando resposta por string em json_decode (opcional)
+// Executa a chamada à API
+try {
+    $response = $open_ai->complete($params);
+} catch (Exception $e) {
+    echo 'Erro ao consultar a API: ' . $e->getMessage();
+    exit;
+}
 
-$texto = getStr($complete,'"text":"','"'); //pegando a resposta
+// Extrai a resposta da API
+$texto = get_string_between($response, '"text":"', '"');
 
-print($texto); //recebendo a resposta
+// Imprime a resposta
+echo $texto;
 
-
-/*BOM USO!
-
-💻 coded by: @EUTHEUZIN
-meu canal no telegram: @CardersDo7
- 
-ajudante com key: @pladixoficial 
-canal do pladix no telegram: @materialdosvideos
-*/
-?>
+/**
+ * Função para extrair uma substring entre duas outras substrings.
+ */
+function get_string_between($string, $start, $end) {
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
